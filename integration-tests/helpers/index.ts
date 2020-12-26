e06d726f6a4c54547c4957c96e58a41222c9342d
Jakub Jirutka <jakub@jirutka.cz>
2020-12-26 18:42:28 +0100

    JS: Add integration tests

---
+++ integration-tests/helpers/index.ts
@@ -0,0 +1,28 @@
+import * as FS from 'fs'
+import * as http from 'http'
+import finalHandler from 'finalhandler'
+import * as OS from 'os'
+import serveStatic from 'serve-static'
+
+
+export function createStaticServer (root: string, options?: serveStatic.ServeStaticOptions): http.Server {
+  const serve = serveStatic(root, options)
+
+  return http.createServer((req, res) => {
+    serve(req, res, finalHandler(req, res) as () => void)
+  })
+}
+
+export function isFile (pathname: string): boolean {
+  try {
+    return FS.statSync(pathname).isFile()
+  } catch {
+    return false
+  }
+}
+
+export const mktempd = (suffix: string) => FS.mkdtempSync(`${OS.tmpdir()}/${suffix}`)
+
+export function readJson (filename: string): any {
+  return JSON.parse(FS.readFileSync(filename, 'utf8'))
+}
---
+++ integration-tests/index.test.ts
@@ -0,0 +1,60 @@
+import { assert } from 'chai'
+import FS from 'fs'
+import getPort from 'get-port'
+import { after, before, test } from 'mocha'
+import { basename } from 'path'
+
+import { createStaticServer, isFile, mktempd, readJson } from './helpers'
+import { IndexEntry, NginxBinary } from '../src'
+import { hostArch, hostOs } from '../src/internal/host'
+
+
+const fixtureRepoPath = `${__dirname}/fixtures/repo`
+const server = createStaticServer(fixtureRepoPath)
+let tempDir: string
+
+before(async () => {
+  const host = '127.0.0.1'
+  const port = await getPort({ host })
+
+  server.listen(port, host)
+  tempDir = mktempd('nginx-binaries')
+
+  NginxBinary.repoUrl = `http://${host}:${port}/`
+})
+
+after(() => {
+  server.close()
+  FS.rmSync(tempDir, { force: true, recursive: true })
+})
+
+describe('NginxBinary', () => {
+
+  test('.download', async () => {
+    const result = await NginxBinary.download({ version: '1.18.0', os: 'linux', arch: 'x86_64' }, tempDir)
+
+    assert(isFile(result), 'Expected the returned path to be a file.')
+
+    const actualFile = FS.readFileSync(result)
+    const expectedFile = FS.readFileSync(`${fixtureRepoPath}/${basename(result)}`)
+
+    assert(Buffer.compare(actualFile, expectedFile) === 0,
+      'Expected the correct file to be fetched.')
+  })
+
+  test('.search', async () => {
+    const indexJson = readJson(`${fixtureRepoPath}/index.json`).contents as IndexEntry[]
+    const expected = indexJson
+      .filter(e => e.name === 'nginx' && e.arch === hostArch && e.os === hostOs)
+
+    assert.sameDeepMembers(await NginxBinary.search({ version: '>=1.18.0' }), expected)
+  })
+
+  test('.variants', async () => {
+    assert.deepEqual(await NginxBinary.variants(), [''])
+  })
+
+  test('.versions', async () => {
+    assert.deepEqual(await NginxBinary.versions(), ['1.19.5', '1.18.0'])
+  })
+})
