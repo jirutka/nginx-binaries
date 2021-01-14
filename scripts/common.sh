7f910a72ac3951d5953e84c4c48beee9a3322d5e
Jakub Jirutka <jakub@jirutka.cz>
2021-01-15 00:33:51 +0100

    CI: Move common functions from scripts to common.sh

---
+++ scripts/common.sh
@@ -0,0 +1,10 @@
+case "$(uname -s)" in
+	Darwin)
+		filesize() { stat -f %z "$1"; }
+		checksum() { shasum -a 256 "$1" | cut -d' ' -f1; }
+	;;
+	*)
+		filesize() { stat -c %s "$1"; }
+		checksum() { sha256sum "$1" | cut -d' ' -f1; }
+	;;
+esac
