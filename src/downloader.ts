bb43abafc0d331b41c1031226a88e3e1f269b595
Jakub Jirutka <jakub@jirutka.cz>
2021-01-15 19:37:00 +0100

    JS: Split index.ts into downloader.ts and repoIndex.ts

---
+++ src/downloader.ts
@@ -0,0 +1,120 @@
+import OS from 'os'
+import path from 'path'
+
+import { formatQuery, getIndex, queryIndex, FORMAT_VERSION, IndexEntry, Query } from './repoIndex'
+import { normalizeArch } from './internal/archName'
+import { getCacheDir } from './internal/cacheDir'
+import { downloadFile } from './internal/downloadFile'
+import { bindAll, replaceProperty } from './internal/utils'
+
+
+const defaultQuery: Omit<Required<Query>, 'version'> = {
+  // macOS (darwin) on ARM can run x86_64 binaries.
+  arch: OS.platform() === 'darwin' ? 'x86_64' : normalizeArch(OS.arch()) as any,
+  os: OS.platform() as any,
+  variant: '',
+}
+
+// NOTE: Keep in sync with API section in README.adoc (until I figure out how to generate it).
+export interface Downloader {
+  /**
+   * Path to the cache directory where the repository index and binaries are stored.
+   *
+   * Defaults to `.cache/nginx-binaries/` relative to the nearest writable `node_modules`
+   * (nearest to `process.cwd()`) or `nginx-binaries/` in the system-preferred temp
+   * directory.
+   */
+  cacheDir: string
+  /**
+   * Maximum age in minutes for the cached repository index in `cacheDir` to be
+   * considered fresh. If the cached index is stale, the Downloader tries to refresh
+   * it before reading.
+   *
+   * @default 480 (8 hours)
+   */
+  cacheMaxAge: number
+  /**
+   * URL of the repository with binaries.
+   *
+   * **Caution:** After changing `repoUrl`, you should delete old `index.json` in
+   * `cacheDir` or disable index cache by setting `cacheMaxAge` to `0`.
+   *
+   * @default 'https://jirutka.github.io/nginx-binaries'
+   */
+  repoUrl: string
+  /**
+   * Fetch response timeout in milliseconds.
+   *
+   * @default 10000
+   */
+  timeout: number
+  /**
+   * Downloads a binary specified by `query` and stores it in the `cacheDir` or
+   * in `destFilePath`, if provided. Returns path to the file.
+   *
+   * If the file already exists and the checksums match, it just returns its path.
+   *
+   * If multiple versions satisfies the version range, the one with highest
+   * version number is selected.
+   *
+   * @param query A query that specifies what binary to download.
+   * @param destFilePath An optional file path where to write the downloaded binary.
+   * @return Path to the downloaded binary on the filesystem.
+   * @throws {RangeError} if no matching binary is found.
+   */
+  download: (query: Query, destFilePath?: string) => Promise<string>
+  /**
+   * Returns metadata of available binaries that match the query.
+   */
+  search: (query: Query) => Promise<IndexEntry[]>
+  /**
+   * Returns all the available variants matching the query.
+   */
+  variants: (query?: Query) => Promise<string[]>
+  /**
+   * Returns all the available versions matching the query.
+   */
+  versions: (query?: Query) => Promise<string[]>
+}
+
+export const createDownloader = (name: string): Downloader => bindAll({
+  cacheMaxAge: 8 * 60,  // minutes
+  repoUrl: 'https://jirutka.github.io/nginx-binaries',
+  timeout: 10_000,  // milliseconds
+
+  get cacheDir () {
+    // Replace accessors with plain value (lazy initialization).
+    return replaceProperty(this, 'cacheDir', getCacheDir('nginx-binaries'))
+  },
+  set cacheDir (dirpath) {
+    this.cacheDir &&= dirpath
+  },
+
+  async download (query, destFilePath) {
+    const [entry, ] = await this.search(query)
+    if (!entry) {
+      throw RangeError(`No ${name} binary found for ${formatQuery({ ...defaultQuery, ...query })}`)
+    }
+    destFilePath ??= path.join(this.cacheDir, entry.filename)
+    const url = `${this.repoUrl}/${entry.filename}`
+
+    return await downloadFile(url, entry.checksum, destFilePath, { timeout: this.timeout })
+  },
+
+  async search (query) {
+    const index = await getIndex(this.repoUrl, this.cacheDir, this.timeout, this.cacheMaxAge)
+    // TODO: Move to getIndex()
+    if (index.formatVersion !== FORMAT_VERSION) {
+      throw Error(`Index format version mismatch, clean cache and update nginx-binaries package`)
+    }
+    return queryIndex(index, name, { ...defaultQuery, ...query })
+  },
+
+  async variants (query) {
+    return [...new Set((await this.search(query ?? {})).map(x => x.variant))]
+  },
+
+  async versions (query) {
+    return [...new Set((await this.search(query ?? {})).map(x => x.version))]
+  },
+})
---
+++ src/repoIndex.ts
@@ -0,0 +1,165 @@
+import AnyLogger from 'anylogger'
+import FS from 'fs'
+import { FetchError } from 'node-fetch'
+import path from 'path'
+import semver from 'semver'
+
+import { normalizeArch } from './internal/archName'
+import { fetchJson } from './internal/fetch'
+
+
+const log = AnyLogger('nginx-binaries')
+
+/**
+ * Expected version of the index file.
+ * Keep in sync with FORMAT_VERSION in scripts/generate-index!
+ */
+export const FORMAT_VERSION = 2
+
+export interface IndexFile {
+  formatVersion: number
+  contents: IndexEntry[]
+}
+
+// NOTE: Keep in sync with API section in README.adoc (until I figure out how to generate it).
+export interface IndexEntry {
+  /**
+   * Name of the program (`nginx` or `njs`).
+   */
+  name: string
+  /**
+   * Version of the program.
+   */
+  version: string
+  /**
+   * The build variant of the binary (e.g. `debug`).
+   * An empty string indicates the default variant.
+   */
+  variant: string
+  /**
+   * OS platform for which this binary was built.
+   */
+  os: Platform
+  /**
+   * CPU architecture for which this binary was built.
+   */
+  arch: Arch
+  /**
+   * Full name of the binary file.
+   */
+  filename: string
+  /**
+   * Date and time (ISO-8601) at which the binary was built.
+   */
+  date: string
+  /**
+   * Size of the binary file in bytes.
+   */
+  size: number
+  /**
+   * Checksum of the binary file in format `<algorithm>:<hash>`.
+   *
+   * @example 'sha1:7336b675b26bd67fdda3db18c66fa7f64691e280'
+   */
+  checksum: string
+  /**
+   * A record of all libraries (or modules) statically linked into the binary
+   * and the version number.
+   *
+   * @example
+   * {
+   *   'openssl': '1.1.1i-r0',
+   *   'echo-nginx-module': '0.62',
+   * }
+   */
+  bundledLibs: Record<string, string>
+}
+
+type Platform = 'linux' | 'darwin' | 'win32'
+type Arch = 'armv7' | 'arm' | 'aarch64' | 'arm64' | 'ppc64le' | 'x86_64' | 'x64'
+
+// NOTE: Keep in sync with API section in README.adoc (until I figure out how to generate it).
+export interface Query {
+  /**
+   * Specify required version as exact version number or a SemVer version range.
+   *
+   * @example '1.8.0', '1.8.x', '^1.8.0'
+   * @see https://github.com/npm/node-semver#ranges
+   */
+  version?: string
+  /**
+   * Specify build variant of the binary (e.g. `debug`).
+   * Defaults to an empty string, i.e. the default variant.
+   */
+  variant?: string
+  /**
+   * Specify target OS. Defaults to the host OS.
+   */
+  os?: Platform
+  /**
+   * Specify target CPU architecture. Defaults to the host architecture.
+   */
+  arch?: Arch
+}
+
+export async function getIndex (
+  repoUrl: string,
+  cacheDir: string,
+  fetchTimeout: number,
+  cacheMaxAge: number,
+): Promise<IndexFile> {
+  const cachedIndexPath = path.join(cacheDir, 'index.json')
+
+  const readCachedIndex = () => JSON.parse(FS.readFileSync(cachedIndexPath, 'utf8')) as IndexFile
+
+  let isCached = false
+  try {
+    if (Date.now() - FS.statSync(cachedIndexPath).mtimeMs < cacheMaxAge * 60_000) {
+      log.debug(`Using cached index ${cachedIndexPath}`)
+      return readCachedIndex()
+    }
+    isCached = true
+  } catch {
+    // ignore
+  }
+  try {
+    log.debug(`Fetching ${repoUrl}/index.json`)
+
+    const index = await fetchJson(`${repoUrl}/index.json`, { timeout: fetchTimeout }) as IndexFile
+
+    FS.mkdirSync(cacheDir, { recursive: true })
+    FS.writeFileSync(cachedIndexPath, JSON.stringify(index, null, 2))
+
+    return index
+
+  } catch (err) {
+    if (isCached && err instanceof FetchError && err.type === 'system') {
+      log.warn('Failed to refresh repository index, using stale index')
+      return readCachedIndex()
+    }
+    throw err
+  }
+}
+
+export function queryIndex (index: IndexFile, name: string, query: Query): IndexEntry[] {
+  log.debug(`Looking for ${name} binary matching ${formatQuery(query)}`)
+
+  return index.contents
+    .filter(x => x.name === name)
+    .filter(queryFilter(query))
+    .sort((a, b) => semver.rcompare(a.version, b.version))
+}
+
+export const formatQuery = (query: Query) => JSON.stringify(query)
+  .replace(/"/g, '')
+  .replace(/,/g, ', ')
+  .replace(/:/g, ': ')
+
+const queryFilter = (query: Query) => (meta: IndexEntry) => objKeys(query).every(key => {
+  return query[key] === undefined ? false
+    : key === 'version' ? semver.satisfies(meta[key], query[key]!)
+    : key === 'arch' ? normalizeArch(query[key]!) === meta[key]
+    : query[key] === meta[key]
+})
+
+const objKeys = <T> (obj: T) => Object.keys(obj) as Array<keyof T>
