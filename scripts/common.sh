f55592bee7e541943f9f2384245616d21d358a9c
Jakub Jirutka <jakub@jirutka.cz>
2021-10-20 19:20:44 +0200

    CI: Use gsed on macOS
    sed(1) on macOS behaves weird. The following command didn't work
    (printed nothing) and I couldn't figure out why.
    
        NJS_CFLAGS=$(sed -n 's/^NJS_CFLAGS\s*=\s*//p' build/Makefile)

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,6 +2,7 @@ case "$(uname -s)" in
 	Darwin)
 		filesize() { stat -f %z "$1"; }
 		checksum() { shasum -a 256 "$1" | cut -d' ' -f1; }
+		alias sed='gsed'
 	;;
 	*)
 		filesize() { stat -c %s "$1"; }
